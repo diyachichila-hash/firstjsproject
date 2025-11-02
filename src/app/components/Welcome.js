807ef66fe580f017a488b3d41f2324e78f20f175
diyachichila-hash <diyachichila@gmail.com>
2025-11-01 19:51:50 -0700

    inital command

---
+++ src/app/components/Welcome.js
@@ -0,0 +1,12 @@
+export default function Welcome() {
+    return (
+        <div className="flex flex-col items-center justify-center text-center">
+            <h1 className ="font-bold text-[50px] text-blue-950">
+                wow new website
+            </h1>
+            <p className="text-gray-500 text-[25px]">
+                this is the first react component, called Welcome.js
+            </p>
+        </div>
+    )
+}
