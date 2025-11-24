560909690a6c190c0711400a1af5d8df3ce8e9fe
diyachichila-hash <diyachichila@gmail.com>
2025-11-23 18:07:33 -0800

    image and counter changes

--- src/app/components/Counter.js
+++ src/app/components/Counter.js
@@ -1,23 +1,28 @@
 "use client"
+
 import { useState } from "react";
 
-export default function Counter() {
-    const [count, setCount] = useState(0);
 
+export default function Counter({increment, startAt, title}) {
+    const [currentNumber, setCurrentNumber] =  useState(startAt);
     function handleClick() {
-        setCount(count +1);
+        setCurrentNumber(currentNumber + increment);
     }
 
     return (
         <div className="text-center mt-5">
-            <h2>
-                react Counter
-            </h2>
-            <p> 
-                you clicked {count} times!
-            </p>
+            <h2>React Counter {title}</h2>
+            {increment > 2 ? (
+                <p>The increment value is more than 2.</p>
+            ): null}
+            {startAt > 0 ? (
+                <p>
+                    The starting value is more than 0.
+                </p>
+            ): null}
+            <p>You clicked {currentNumber} times!</p>
             <button onClick={handleClick}>
-                click me to change the count
+                Click me to change the count!
             </button>
         </div>
     )
---
+++ src/app/components/Image.js
@@ -0,0 +1,10 @@
+"use client"
+
+import { useState } from "react";
+
+
+export default function Image({imageUrl}) {
+    return(
+        <img src={imageUrl} />
+    )
+}
